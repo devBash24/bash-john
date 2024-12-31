e910fd273e41d177f83bc348ca99dd9226844945
Bash John <138396242+devBash24@users.noreply.github.com>
2024-12-31 19:15:51 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-3xl font-bold underline bg-red-500 h-screen w-screen">
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
